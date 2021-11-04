e8f3d6d656ad8de16ac97105731bec9067b6e751
Derick Vigne <derickvigne@me.com>
2021-11-03 23:53:43 -0400

    Added Vue Processor Directive to Mix -- Disabled Browsersync from Earlier Development

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,16 @@
 let mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
-mix.sass('src/css/index.scss', 'public/css/index.css')
+mix.js('/src/js/render/index.js', 'public/js/index.js')
+   .vue()
+   .sass('src/css/index.scss', 'public/css/index.css')
    .options({
      postCss: [ tailwindcss('./tailwind.config.js') ],
    });
 
-mix.browserSync({
-  files: ["src/js/*.js", "src/css/*.scss", "src/html/*.html"],
-  server: {
-    index: "src/html/index.html"
-  }
-});
+// mix.browserSync({
+//   files: ["src/js/*/*.js", "src/js/*/*.vue", "src/css/*.scss", "src/html/*.html"],
+//   server: {
+//     index: "src/html/index.html"
+//   }
+// });
