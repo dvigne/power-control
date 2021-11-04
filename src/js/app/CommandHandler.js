0892bacad92f1161f1119cd29e54b4ec750d0e87
Derick Vigne <derickvigne@me.com>
2021-11-03 23:51:34 -0400

    Added IPC Hooks and Command Handlers for Power Control

---
+++ src/js/app/CommandHandler.js
@@ -0,0 +1,155 @@
+const { ipcMain } = require('electron');
+const net = require('net')
+
+/*
+*IDN? - Get system information
+INSTrument {CH1|CH2} - Select channel to be operated
+INSTrument ? - Get currently operating channel
+MEASure:CURRent? <CH1|CH2> - Retrieve current level on channel
+MEAsure:VOLTage? <CH1|CH2> - Retrieve voltage level on channel
+[SOURce:]CURRent <current> - Set current on operating channel
+[SOURce(<CH1|CH2>):]CURRent? - Get current output on channel
+[SOURce:]VOLTage <volt> - Set voltage on operating channel
+[SOURce(<CH1|CH2>):]VOLTage?- Get current voltage output on operating channel
+OUTPut <source>,<state - Set source (CH1, CH2, Ch3) to state (ON, OFF)
+OUTPut:TRACk <NR1> - Set track operation (0 = independant, 1 = series, 2 = parallel)
+SYSTem:ERRor? - Get system errors
+SYSTem:VERSion? - Get software version
+SYSTem: STATus? - Get system status
+*/
+
+var client = new net.Socket();
+
+function handleData(cb) {
+  client.on('data', cb);
+}
+
+ipcMain.on('connect', function(event, args) {
+  console.log(`Connected to: ${args[1]} on port ${args[0]}`)
+  client.on('error', function(error) {
+    event.returnValue = [false, error];
+  });
+  client.connect(args[0], args[1], function() {
+    event.returnValue = [true];
+  });
+});
+
+ipcMain.on('disconnect', function(event, args) {
+  client.disconnect();
+});
+
+ipcMain.on('systemInfo', function(event, args) {
+  console.log("Fetching System Info");
+  client.write("*IDN?", function() {
+    handleData(function(data) {
+      console.log(data);
+      event.returnValue = data.toString();
+    })
+  })
+});
+
+ipcMain.on('setCurrentChannel', function(event, args) {
+  client.write(`INST ${args}`, function() {
+    event.returnValue = null;
+  });
+});
+
+ipcMain.on('getCurrentChannel', function(event, args) {
+  client.write(`INST?`, function() {
+    handleData(function(response) {
+      event.returnValue = response.toString();
+    });
+  });
+});
+
+ipcMain.on('measureCurrent', function(event, args) {
+  client.write(`MEAS:CURR? ${args}`, function() {
+    handleData(function(response) {
+      event.returnValue = parseFloat(response);
+    });
+  });
+});
+
+ipcMain.on('measureVoltage', function(event, args) {
+  client.write(`MEAS:VOLT? ${args}`, function() {
+    handleData(function(response) {
+      event.returnValue = parseFloat(response);
+    });
+  });
+});
+
+ipcMain.on('setCurrent', function(event, args) {
+  client.write(`${args[0]}:CURR ${args[1]}`, function() {
+    event.returnValue = null;
+  });
+});
+
+ipcMain.on('getCurrent', function(event, args) {
+  client.write(`${args}:CURR?`, function() {
+    handleData(function(response) {
+      event.returnValue = parseFloat(response);
+    });
+  });
+});
+
+ipcMain.on('setVoltage', function(event, args) {
+  client.write(`${args[0]}:VOLT ${args[1]}`, function() {
+    event.returnValue = null;
+  });
+});
+
+ipcMain.on('getVoltage', function(event, args) {
+  client.write(`${args}:VOLT?`, function() {
+    handleData(function(response) {
+      event.returnValue = parseFloat(response);
+    });
+  });
+});
+
+ipcMain.on('setOutput', function(event, args) {
+  client.write(`OUTP ${args[0]},${args[1]}`, function() {
+    event.returnValue = null;
+  });
+});
+
+ipcMain.on('setTrack', function(event, args) {
+  client.write(`OUTP:TRACK ${args}`, function() {
+    event.returnValue = null;
+  });
+});
+
+ipcMain.on('errors', function(event, args) {
+  client.write("SYST:ERR?", function() {
+    handleData(function(response) {
+      event.returnValue = response.toString();
+    });
+  });
+});
+
+ipcMain.on('version', function(event, args) {
+  client.write("SYST:VERS?", function() {
+    handleData(function(response) {
+      event.returnValue = response.toString();
+    });
+  });
+});
+
+ipcMain.on('status', function(event, args) {
+  client.write("SYST:VERS?", function() {
+    handleData(function(response) {
+      event.returnValue = parseInt(response.toString(), 16);
+    });
+  });
+});
+
+ipcMain.on('lock', function(event, args) {
+  client.write("*LOCK", function() {
+    event.returnValue = null;
+  });
+});
+
+ipcMain.on('unlock', function(event, args) {
+  client.write("*UNLOCK", function() {
+    event.returnValue = null;
+  });
+});
---
+++ src/js/app/preload.js
@@ -0,0 +1,59 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld("psu",
+{
+  connect: function(host, port) {
+    return ipcRenderer.sendSync('connect', [host, port]);
+  },
+  disconnect: function() {
+    ipcRenderer.sendSync('disconnect');
+  },
+  systemInfo: function() {
+    return ipcRenderer.sendSync('systemInfo');
+  },
+  setCurrentChannel: function(channel) {
+    ipcRenderer.sendSync('setCurrentChannel', channel);
+  },
+  getCurrentChannel: function() {
+    return ipcRenderer.sendSync('getCurrentChannel');
+  },
+  measureCurrent: function(channel) {
+    return ipcRenderer.sendSync('measureCurrent', channel);
+  },
+  measureVoltage: function(channel) {
+    return ipcRenderer.sendSync('measureVoltage', channel);
+  },
+  setCurrent: function(channel, current) {
+    ipcRenderer.sendSync('setCurrent', [channel, current]);
+  },
+  getCurrent: function(channel) {
+    return ipcRenderer.sendSync('getCurrent', channel);
+  },
+  setVoltage: function(channel, current) {
+    ipcRenderer.sendSync('setVoltage', [channel, current]);
+  },
+  getVoltage: function(channel) {
+    return ipcRenderer.sendSync('getVoltage', channel);
+  },
+  setOutput: function(channel, state) {
+    ipcRenderer.sendSync('setOutput', [channel, state]);
+  },
+  setTrack: function(state) {
+    ipcRenderer.sendSync('setTrack', state);
+  },
+  errors: function() {
+    return ipcRenderer.sendSync('errors');
+  },
+  version: function() {
+    return ipcRenderer.sendSync('version');
+  },
+  status: function() {
+    return ipcRenderer.sendSync('status');
+  },
+  lock: function() {
+    return ipcRenderer.sendSync('lock');
+  },
+  unlock: function() {
+    return ipcRenderer.sendSync('unlock');
+  }
+});
