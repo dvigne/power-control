53cdf1e719111230ea2782c7fad37492d57c1501
Derick Vigne <derickvigne@me.com>
2021-11-07 02:21:12 -0500

    Implemented Vue Router + Routes

--- src/js/render/index.js
+++ src/js/render/index.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuelidate from 'vuelidate'
 
-import channelsettings from './ChannelSettings.vue'
-import controlpanel from './ControlPanel.vue'
+Vue.use(VueRouter);
+Vue.use(Vuelidate);
 
-var app = new Vue({
-  el: '#power-control',
-  components: {
-    channelsettings,
-    controlpanel
-  }
+import connectscreen from './components/ConnectScreen'
+import controlscreen from './components/ControlScreen'
+
+const routes = [
+  { path: "/", component: connectscreen},
+  { path: "/control", component: controlscreen}
+];
+
+const router = new VueRouter({
+  routes
 });
+
+const app = new Vue({
+  router
+}).$mount("#power-control");
